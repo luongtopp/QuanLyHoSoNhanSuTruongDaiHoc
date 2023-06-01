50bc9cf79a00f99f0dba67513ddd5873970c2601
ShadyNguyen <113918007+ShadyNguyen@users.noreply.github.com>
2023-06-01 10:55:44 +0700

    duong c1

---
+++ src/UTT/Model/DanhGiaCanBo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UTT.Model;
+
+import UTT.Connection.JdbcHelper;
+import static UTT.Connection.JdbcHelper.executeUpdate;
+import static UTT.Connection.JdbcHelper.preparedStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author HL2020
+ */
+public class DanhGiaCanBo {
+    
+   
+
+}
